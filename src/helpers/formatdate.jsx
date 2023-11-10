63e196e3607c171296cdb5ccb057926378a1b9f0
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-11-10 19:53:25 +0500

    0 adder component

---
+++ src/helpers/formatdate.jsx
@@ -0,0 +1,15 @@
+const addsuffix = num => {
+  return String(num).padStart(2, "0");
+}
+
+const formatdate = (date) => {
+  date = new Date(date);
+  const hours = addsuffix(date.getHours());
+  const minutes = addsuffix(date.getMinutes());
+  const day = addsuffix(date.getDate());
+  const month = addsuffix(date.getMonth() + 1);
+  const year = date.getYear();
+  return ({hours, minutes, day, month, year});
+}
+
+export default formatdate
